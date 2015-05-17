7a82d152c7b17a75795bdaf7de37cab37e4026b1
Maxim Egorov <maxgegorov@gmail.com>
2015-05-17 13:38:27 -0700

    File name changes

---
+++ src/DiscreteValueIteration.jl
@@ -0,0 +1,23 @@
+# implements value iteration solver for MDPs
+
+module DiscreteValueIteration
+
+using DiscreteMDPs
+
+import DiscreteMDPs.Solver
+import DiscreteMDPs.solve
+import DiscreteMDPs.Policy
+import DiscreteMDPs.action
+import DiscreteMDPs.value
+
+include("policy.jl")
+include("serial.jl")
+include("parallel.jl")
+
+export ParallelSolver
+export SerialSolver
+export solve
+export DiscretePolicy
+export action, value
+
+end # module
