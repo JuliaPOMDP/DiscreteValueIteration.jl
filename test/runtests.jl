2410b6977364fd28e0eb8323ce8e65a83ed2c7e4
Pamela Toman <ptoman@stanford.edu>
2016-12-04 09:33:34 -0800

    Finishes cleaning up testing environment

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,16 +39,53 @@ function test_creation_of_policy_given_utilities()
 	# GridWorld:
 	# |_________|_________|_________| plus a fake state for absorbing
 	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	
 	# Create a starter set of utilities 
 	# 0.0 is associated with the fake absorbing state
-	utility = [5.45632, 8.20505, 10.0, 0.0] 
+	correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
+
+	# Derive a policy & check that they match	
 	policy = ValueIterationPolicy(mdp, utility=utility, include_Q=true)
+	println( policy)
+	
 	return policy.util == utility
 end
 
 
+function test_creation_of_policy_given_q_util_policy()
+	# GridWorld:
+	# |_________|_________|_________| plus a fake state for absorbing
+	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+	# Create a starter set of utilities 
+	# 0.0 is associated with the fake absorbing state
+	correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
+
+	# Derive a policy & check that they match	
+	policy = ValueIterationPolicy(mdp, utility=utility, include_Q=true)
+	return policy.util == utility
+end
+
+    function ValueIterationPolicy(mdp::Union{MDP,POMDP}, q::Matrix{Float64}, util::Vector{Float64}, policy::Vector{Int64})
+        self = new()
+        self.qmat = q
+        self.util = util
+        self.policy = policy
+        am = Any[]
+        for a in iterator(actions(mdp))
+            aidx = action_index(mdp, a)
+            push!(am, aidx)
+        end
+        self.action_map = am
+        self.include_Q = true
+        self.mdp = mdp
+        return self
+    end
+	
+
 @test test_complex_gridworld() == true
 @test test_creation_of_policy_given_utilities() == true
+@test test_creation_of_policy_given_q_util_policy() == true
 
 println("Finished tests")
+
+
+
