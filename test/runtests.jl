c9a80f9e710243fa9130797da99c14d905d0dea6
Pamela Toman <ptoman@stanford.edu>
2016-12-04 10:05:56 -0800

    Finishes a very basic test for all the implemented ValueIterationPolicy constructors.

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,17 +48,7 @@ function test_creation_of_policy_given_utilities()
 
 	# Derive a policy & check that they match	
 	policy = ValueIterationPolicy(mdp, utility=correct_utility, include_Q=true)
-	
-	
-    solver = ValueIterationSolver(max_iterations=niter, belres=res)
-    policy = solve(solver, mdp, policy, verbose=true)
-	
-	println( policy.util)
-	println( policy.qmat)
-	println( policy.policy)
-	println( policy.include_Q)
-	
-	return policy.util == correct_utility
+	return policy.mdp == mdp && policy.util == correct_utility
 end
 
 
@@ -69,36 +59,34 @@ function test_creation_of_policy_given_q_util_policy()
 	# GridWorld:
 	# |_________|_________|_________| plus a fake state for absorbing
 	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	# Create a starter set of utilities 
-	# 0.0 is associated with the fake absorbing state
 	correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
-
+	correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
+	correct_policy = [1,1,1,1]
+	
 	# Derive a policy & check that they match	
-	policy = ValueIterationPolicy(mdp, utility=utility, include_Q=true)
-	return policy.util == correct_utility
+	policy = ValueIterationPolicy(mdp, correct_qmat, correct_utility, correct_policy)
+	return (policy.mdp == mdp && policy.qmat == correct_qmat && policy.util == correct_utility && policy.policy == correct_policy && policy.include_Q == true)
 end
 
-#=
-    function ValueIterationPolicy(mdp::Union{MDP,POMDP}, q::Matrix{Float64}, util::Vector{Float64}, policy::Vector{Int64})
-        self = new()
-        self.qmat = q
-        self.util = util
-        self.policy = policy
-        am = Any[]
-        for a in iterator(actions(mdp))
-            aidx = action_index(mdp, a)
-            push!(am, aidx)
-        end
-        self.action_map = am
-        self.include_Q = true
-        self.mdp = mdp
-        return self
-    end
-=#	
+function test_creation_of_policy_given_q()
+	# If we create a ValueIterationPolicy by providing a default q,
+	# does its value successfully get saved?
+	#
+	# GridWorld:
+	# |_________|_________|_________| plus a fake state for absorbing
+	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+	correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
+	
+	# Derive a policy & check that they match	
+	policy = ValueIterationPolicy(mdp, correct_qmat)
+	return (policy.mdp == mdp && policy.qmat == correct_qmat)
+end
+
 
 @test test_complex_gridworld() == true
 @test test_creation_of_policy_given_utilities() == true
-#@test test_creation_of_policy_given_q_util_policy() == true
+@test test_creation_of_policy_given_q_util_policy() == true
+@test test_creation_of_policy_given_q() == true
 
 println("Finished tests")
 
