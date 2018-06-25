e430f69439aa98f8f358289d73a4088479761a92
MaximeBouton <maxim.bouton@gmail.com>
2018-06-25 10:18:23 +0200

    tests

--- test/runtests_parallel.jl
+++ test/runtests_parallel.jl
@@ -1,12 +1,12 @@
-addprocs(2)
+# addprocs(2)
 @everywhere using DiscreteValueIteration, POMDPs, POMDPModels
 
-mdp = GridWorld(sx=500, sy=500)
+mdp = GridWorld(sx=1000, sy=1000)
 
 solver = ParallelValueIterationSolver(n_procs=3)
 
 parallel_policy = solve(solver, mdp, verbose=true)
 
-serial_policy = solve(ValueIterationSolver(), mdp)
+serial_policy = solve(ValueIterationSolver(), mdp, verbose=true)
 
 @test isapprox(norm(parallel_policy.util - serial_policy.util), 0., atol=solver.belres)
