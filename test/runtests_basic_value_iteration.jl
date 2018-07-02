f12c0e3a9f2e9a1f56c71ba577c35660937c38c5
rejuvyesh <mail@rejuvyesh.com>
2018-07-02 11:21:09 -0700

    untabify

--- test/runtests_basic_value_iteration.jl
+++ test/runtests_basic_value_iteration.jl
@@ -16,71 +16,71 @@ end
 
 
 function test_complex_gridworld()
-	# Load correct policy from file and verify we can reconstruct it
-	rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
-	rvals = [-10.0, -5.0, 10.0, 3.0]
-	xs = 10
-	ys = 10
-	mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
-	file = "grid-world-10x10-Q-matrix.txt"
-	niter = 100
-	res = 1e-3
+    # Load correct policy from file and verify we can reconstruct it
+    rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
+    rvals = [-10.0, -5.0, 10.0, 3.0]
+    xs = 10
+    ys = 10
+    mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
+    file = "grid-world-10x10-Q-matrix.txt"
+    niter = 100
+    res = 1e-3
 
-	return support_serial_qtest(mdp, file, niter=niter, res=res)
+    return support_serial_qtest(mdp, file, niter=niter, res=res)
 end
 
 function test_simple_grid()
-	# Simple test....
-	# GridWorld(sx=2,sy=3) w reward at (2,3):
-	# Here's our grid:
-	# |state (x,y)____available actions__|
-	# ----------------------------------------------
-	# |5 (1,3)__u,d,l,r__|6 (2,3)__u,d,l,r+REWARD__|
-	# |3 (1,2)__u,d,l,r__|4 (2,2)______u,d,l,r_____|
-	# |1 (1,1)__u,d,l,r__|2 (2,1)______u,d,l,r_____|
-	# ----------------------------------------------
-	# 7 (0,0) is absorbing state
-	mdp = GridWorld(sx=2, sy=3, rs = [GridWorldState(2,3)], rv = [10.0])
-	
-	solver = ValueIterationSolver(verbose=true)
+    # Simple test....
+    # GridWorld(sx=2,sy=3) w reward at (2,3):
+    # Here's our grid:
+    # |state (x,y)____available actions__|
+    # ----------------------------------------------
+    # |5 (1,3)__u,d,l,r__|6 (2,3)__u,d,l,r+REWARD__|
+    # |3 (1,2)__u,d,l,r__|4 (2,2)______u,d,l,r_____|
+    # |1 (1,1)__u,d,l,r__|2 (2,1)______u,d,l,r_____|
+    # ----------------------------------------------
+    # 7 (0,0) is absorbing state
+    mdp = GridWorld(sx=2, sy=3, rs = [GridWorldState(2,3)], rv = [10.0])
+    
+    solver = ValueIterationSolver(verbose=true)
     policy = solve(solver, mdp)
 
-	# up: 1, down: 2, left: 3, right: 4
-	correct_policy = [1,1,1,1,4,1,1] # alternative policies
-	# are possible, but since they are tied & the first 
-	# action is always 1, we will always return 1 for tied
-	# actions
-	return policy.policy == correct_policy
+    # up: 1, down: 2, left: 3, right: 4
+    correct_policy = [1,1,1,1,4,1,1] # alternative policies
+    # are possible, but since they are tied & the first 
+    # action is always 1, we will always return 1 for tied
+    # actions
+    return policy.policy == correct_policy
 end
 
 function test_init_solution()
-	# Initialize the value to the solution 
-	rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
-	rvals = [-10.0, -5.0, 10.0, 3.0]
-	xs = 10
-	ys = 10
-	mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
-	qt = readdlm("grid-world-10x10-Q-matrix.txt")
-	ut = maximum(qt, 2)[:]
-	solver = ValueIterationSolver(verbose=true, init_util=ut, belres=1e-3)
-	policy = solve(solver, mdp)
-	return isapprox(ut, policy.util, rtol=1e-5)
+    # Initialize the value to the solution 
+    rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
+    rvals = [-10.0, -5.0, 10.0, 3.0]
+    xs = 10
+    ys = 10
+    mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
+    qt = readdlm("grid-world-10x10-Q-matrix.txt")
+    ut = maximum(qt, 2)[:]
+    solver = ValueIterationSolver(verbose=true, init_util=ut, belres=1e-3)
+    policy = solve(solver, mdp)
+    return isapprox(ut, policy.util, rtol=1e-5)
 end
 
 function test_not_include_Q()
-	# Load correct policy from file and verify we can reconstruct it
-	rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
-	rvals = [-10.0, -5.0, 10.0, 3.0]
-	xs = 10
-	ys = 10
-	mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
-	qt = readdlm("grid-world-10x10-Q-matrix.txt")
-	ut = maximum(qt, 2)[:]
-	niter = 100
-	res = 1e-3
-	solver = ValueIterationSolver(verbose=true, init_util=ut, belres=1e-3, include_Q=false)
-	policy = solve(solver, mdp)
-	return isapprox(ut, policy.util, rtol=1e-3)
+    # Load correct policy from file and verify we can reconstruct it
+    rstates = [GridWorldState(4,3), GridWorldState(4,6), GridWorldState(9,3), GridWorldState(8,8)]
+    rvals = [-10.0, -5.0, 10.0, 3.0]
+    xs = 10
+    ys = 10
+    mdp = GridWorld(sx=xs, sy=ys, rs = rstates, rv = rvals)
+    qt = readdlm("grid-world-10x10-Q-matrix.txt")
+    ut = maximum(qt, 2)[:]
+    niter = 100
+    res = 1e-3
+    solver = ValueIterationSolver(verbose=true, init_util=ut, belres=1e-3, include_Q=false)
+    policy = solve(solver, mdp)
+    return isapprox(ut, policy.util, rtol=1e-3)
 end
 
 @test test_complex_gridworld() == true
--- test/runtests_value_iteration_policy.jl
+++ test/runtests_value_iteration_policy.jl
@@ -1,61 +1,61 @@
 
 function test_creation_of_policy_given_utilities()
-	# If we create a ValueIterationPolicy by providing a utility,
-	# does that utility successfully get saved?
-	#
-	# GridWorld:
-	# |_________|_________|_________| plus a fake state for absorbing
-	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	# Create a starter set of utilities 
-	# 0.0 is associated with the fake absorbing state
-	correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
+    # If we create a ValueIterationPolicy by providing a utility,
+    # does that utility successfully get saved?
+    #
+    # GridWorld:
+    # |_________|_________|_________| plus a fake state for absorbing
+    mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+    # Create a starter set of utilities 
+    # 0.0 is associated with the fake absorbing state
+    correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
 
-	# Derive a policy & check that they match	
-	policy = ValueIterationPolicy(mdp, utility=correct_utility, include_Q=true)
-	return policy.mdp == mdp && policy.util == correct_utility
+    # Derive a policy & check that they match  
+    policy = ValueIterationPolicy(mdp, utility=correct_utility, include_Q=true)
+    return policy.mdp == mdp && policy.util == correct_utility
 end
 
 
 function test_creation_of_policy_given_q_util_policy()
-	# If we create a ValueIterationPolicy by providing a utility, q, and policy,
-	# does those values successfully get saved?
-	#
-	# GridWorld:
-	# |_________|_________|_________| plus a fake state for absorbing
-	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
-	correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
-	correct_policy = [1,1,1,1]
-	
-	# Derive a policy & check that they match	
-	policy = ValueIterationPolicy(mdp, correct_qmat, correct_utility, correct_policy)
-	return (policy.mdp == mdp && policy.qmat == correct_qmat && policy.util == correct_utility && policy.policy == correct_policy && policy.include_Q == true)
+    # If we create a ValueIterationPolicy by providing a utility, q, and policy,
+    # does those values successfully get saved?
+    #
+    # GridWorld:
+    # |_________|_________|_________| plus a fake state for absorbing
+    mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+    correct_utility = [5.45632, 8.20505, 10.0, 0.0] 
+    correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
+    correct_policy = [1,1,1,1]
+    
+    # Derive a policy & check that they match  
+    policy = ValueIterationPolicy(mdp, correct_qmat, correct_utility, correct_policy)
+    return (policy.mdp == mdp && policy.qmat == correct_qmat && policy.util == correct_utility && policy.policy == correct_policy && policy.include_Q == true)
 end
 
 function test_creation_of_policy_given_q()
-	# If we create a ValueIterationPolicy by providing a default q,
-	# does its value successfully get saved?
-	#
-	# GridWorld:
-	# |_________|_________|_________| plus a fake state for absorbing
-	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
-	
-	# Derive a policy & check that they match	
-	policy = ValueIterationPolicy(mdp, correct_qmat)
-	return (policy.mdp == mdp && policy.qmat == correct_qmat)
+    # If we create a ValueIterationPolicy by providing a default q,
+    # does its value successfully get saved?
+    #
+    # GridWorld:
+    # |_________|_________|_________| plus a fake state for absorbing
+    mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+    correct_qmat = [5.45636 5.1835 5.1835 5.1835; 8.20506 6.47848 7.7948 7.7948; 10.0 10.0 10.0 10.0; 0.0 0.0 0.0 0.0]
+    
+    # Derive a policy & check that they match  
+    policy = ValueIterationPolicy(mdp, correct_qmat)
+    return (policy.mdp == mdp && policy.qmat == correct_qmat)
 end
 
 function test_creation_of_policy_given_policy()
-		# If we create a ValueIterationPolicy by providing a policy,
-	# does those values successfully get saved?
-	#
-	# GridWorld:
-	# |_________|_________|_________| plus a fake state for absorbing
-	mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
-	correct_policy = [1,1,1,1]
-	policy = ValueIterationPolicy(mdp, policy=correct_policy)
-	return policy.policy == correct_policy
+    # If we create a ValueIterationPolicy by providing a policy,
+    # does those values successfully get saved?
+    #
+    # GridWorld:
+    # |_________|_________|_________| plus a fake state for absorbing
+    mdp = GridWorld(sx=1, sy=3, rs = [GridWorldState(1,3)], rv = [10.0])
+    correct_policy = [1,1,1,1]
+    policy = ValueIterationPolicy(mdp, policy=correct_policy)
+    return policy.policy == correct_policy
 end
 
 @test test_creation_of_policy_given_utilities() == true
