8db1a8d9306a02b0b04b4334afeebcab55fd1a55
MaximeBouton <maxim.bouton@gmail.com>
2018-07-25 11:13:44 -0700

    use subhunt

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,6 +1,5 @@
-N_PROCS = 4
+N_PROCS = 56
 addprocs(N_PROCS)
-
 
 using POMDPs
 using POMDPModels
@@ -8,13 +7,13 @@ using SubHunt
 using DiscreteValueIteration
 
 dpomdp = DSubHuntPOMDP(SubHuntPOMDP(), 1.0)
-dpomdp = GridWorld(sx=500, sy=500)
+println("Starting benchmark on Discrete SubHunt POMDP with $(n_states(dpomdp)) states")
 
-pvi_sharedarrays = ParallelValueIterationSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
+# pvi_sharedarrays = ParallelValueIterationSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
 
-pvi_notshared = ParallelValueIterationNotSharedSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
+# pvi_notshared = ParallelValueIterationNotSharedSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
 
 t_shared = @elapsed solve(pvi_sharedarrays, dpomdp)
 t_notshared = @elapsed solve(pvi_notshared, dpomdp)
-
-n_states(dpomdp)
+@sprintf("With states as SharedArray: %.4f", t_shared)
+@sprintf("With states as regular array: %.4f", t_notshared)
