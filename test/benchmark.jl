41c1fcf3ec267a2b68b1b730125982d13fcafff1
MaximeBouton <maxim.bouton@gmail.com>
2018-07-25 17:04:39 -0700

    added @time and verbose

--- test/benchmark.jl
+++ test/benchmark.jl
@@ -1,4 +1,4 @@
-N_PROCS = 28
+N_PROCS = 56
 addprocs(N_PROCS)
 
 using POMDPs
@@ -10,19 +10,18 @@ using DiscreteValueIteration
 dpomdp = DSubHuntPOMDP(SubHuntPOMDP(), 1.0)
 println("Starting benchmark on Discrete SubHunt POMDP with $(n_states(dpomdp)) states")
 
-pvi_sharedarrays = ParallelValueIterationSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
+pvi_sharedarrays = ParallelValueIterationSolver(max_iterations=30, belres=0., verbose=true, n_procs=N_PROCS)
+println("Starting parallel vi with states as shared array")
+@time solve(pvi_sharedarrays, dpomdp)
 
-pvi_notshared = ParallelValueIterationNotSharedSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
+pvi_notshared = ParallelValueIterationNotSharedSolver(max_iterations=30, belres=0., verbose=true, n_procs=N_PROCS)
+println("Starting parallel vi with states as regular array")
+@time solve(pvi_notshared, dpomdp)
 
-pvi_macro = ParallelValueIterationMacroSolver(max_iterations=100, belres=1e-3, verbose=true, n_procs=N_PROCS)
+pvi_macro = ParallelValueIterationMacroSolver(max_iterations=30, belres=0., verbose=true, n_procs=N_PROCS)
+println("Starting parallel vi with @parallel")
+solve(pvi_macro, dpomdp)
 
-vi = ValueIterationSolver(max_iterations=100, belres=1e-3, verbose=true)
-
-t_shared = @elapsed solve(pvi, dpomdp)
-t_notshared = @elapsed solve(pvi_notshared, dpomdp)
-t_macro = @elapsed solve(pvi_macro, dpomdp)
-t_vi = @elapsed solve(vi, dpomdp)
-
-@printf("With states as SharedArray: %.4f", t_shared)
-@printf("With states as regular array: %.4f", t_notshared)
-@printf("With @parallel: %.4f", t_macro)
+vi = ValueIterationSolver(max_iterations=30, belres=0., verbose=true)
+println("Starting serial vi")
+@time solve(vi, dpomdp)
