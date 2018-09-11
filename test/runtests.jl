3218a46c49d253447b022eec38153a5861b8061b
rejuvyesh <mail@rejuvyesh.com>
2018-09-10 17:14:15 -0700

    get back all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,17 @@ using POMDPs
 using Test
 
 # Test basic value iteration functionality
-# include("runtests_value_iteration_policy.jl") # test the policy object
-# include("runtests_basic_value_iteration.jl")  # then the creation of a policy
-# include("runtests_basic_value_iteration_disallowing_actions.jl") # then a complex form where states determine actions
+@testset "all" begin
+@testset "policy" begin
+    include("test_value_iteration_policy.jl") # test the policy object
+end
+@testset "basic" begin
+    include("test_basic_value_iteration.jl")  # then the creation of a policy
+end
+@testset "basic disallowing actions" begin
+    include("test_basic_value_iteration_disallowing_actions.jl") # then a complex form where states determine actions
+end
 
 println("Testing Requirements")
 @requirements_info ValueIterationSolver() GridWorld()
+end
